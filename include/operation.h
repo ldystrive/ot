66397fbfda2de201000877416cb5a3364dfe54c8
ldystrive <594770590@qq.com>
2019-11-29 15:23:02 +0800

    init

---
+++ include/operation.h
@@ -0,0 +1,82 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#include <vector>
+#include <string>
+
+enum class OpType {
+    Insert,
+    Retain,
+    Delete,
+};
+
+class BasicOperation {
+public:
+    BasicOperation();
+    BasicOperation(int);
+    virtual ~BasicOperation();
+    virtual OpType getType() = 0;
+    int length() const;
+private:
+    int len;
+};
+
+class InsertOp : public BasicOperation {
+public:
+    InsertOp();
+    InsertOp(int, std::string);
+    ~InsertOp();
+    OpType getType();
+    std::string getStr() const;
+    InsertOp operator + (const InsertOp&) const;
+private:
+    std::string str;
+};
+
+class RetainOp : public BasicOperation {
+public:
+    RetainOp();
+    RetainOp(int);
+    ~RetainOp();
+    OpType getType();
+    RetainOp operator + (const RetainOp&) const;
+};
+
+class DeleteOp : public BasicOperation {
+public:
+    DeleteOp();
+    DeleteOp(int);
+    ~DeleteOp();
+    OpType getType();
+    DeleteOp operator + (const DeleteOp&) const;
+};
+
+
+class Operation {
+public:
+    Operation();
+    ~Operation();
+    static Operation transform(Operation, Operation);
+
+    bool operator == (const Operation&) const;
+    Operation operator + (const Operation&) const;
+    bool isNoop();
+    
+    Operation operator + (const BasicOperation&) const;
+    
+    Operation addBasicOp(const BasicOperation&);
+    Operation addInsert(const InsertOp&);
+    Operation addDelete(const DeleteOp&);
+    Operation addRetain(const RetainOp&);
+
+    std::string apply(std::string);
+
+
+private:
+    std::vector<BasicOperation> ops;
+    int baseLength;
+    int targetLength;
+};
+
+
+#endif // OPERATION_H
---
+++ src/operation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+#include "operation.h"
+
+BasicOperation::BasicOperation(): len(0) {}
+BasicOperation::BasicOperation(int len): len(len) {}
+BasicOperation::~BasicOperation() {}
+int BasicOperation::length() const{ return len; }
+
+InsertOp::InsertOp(): str("") {}
+InsertOp::InsertOp(int len, std::string str): BasicOperation(len), str(str) {}
+InsertOp::~InsertOp() {}
+OpType InsertOp::getType() { return OpType::Insert; }
+std::string InsertOp::getStr() const{ return str; }
+InsertOp InsertOp::operator + (const InsertOp &op) const{
+    return InsertOp(length() + op.length(), getStr() + op.getStr());
+}
+
+RetainOp::RetainOp() {}
+RetainOp::RetainOp(int len): BasicOperation(len) {}
+RetainOp::~RetainOp() {}
+OpType RetainOp::getType() { return OpType::Retain; }
+RetainOp RetainOp::operator + (const RetainOp &op) const{
+    return RetainOp(length() + op.length());
+}
+
+DeleteOp::DeleteOp() {}
+DeleteOp::DeleteOp(int len): BasicOperation(len) {}
+DeleteOp::~DeleteOp() {}
+OpType DeleteOp::getType() { return OpType::Delete; }
+DeleteOp DeleteOp::operator + (const DeleteOp &op) const{
+    return DeleteOp(length() + op.length());
+}
+
+
+Operation::Operation(): baseLength(0), targetLength(0) {}
+Operation::~Operation() {}
+
+Operation Operation::addInsert(const InsertOp& op) {
+    if (op.length() == 0) return *this;
+    targetLength += op.length();
+    if (!ops.empty() && ops[ops.size()-1].getType() == OpType::Insert) {
+        InsertOp& lastOp = dynamic_cast<InsertOp&>(ops[ops.size()-1]);
+        ops[ops.size()-1] = lastOp + op;
+    }
+    else if (ops.size() > 1 && ops[ops.size()-1].getType() == OpType::Delete) {
+        if (ops[ops.size()-2].getType() == OpType::Insert) {
+            ops[ops.size()-2] = dynamic_cast<InsertOp&>(ops[ops.size()-2]) + op;
+        }
+        else {
+            ops.push_back(ops[ops.size()-1]);
+            ops[ops.size()-2] = op;
+        }
+    }
+    else {
+        ops.push_back(op);
+    }
+    return *this;
+}
+
